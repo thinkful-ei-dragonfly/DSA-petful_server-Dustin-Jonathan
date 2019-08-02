4dfbfe8e13a96ddfe202a03c6a44fad6e65ba32b
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-02 13:12:43 -0500

    Converted arrays to Queue's

--- Petful-server/src/cats/cat-service.js
+++ Petful-server/src/cats/cat-service.js
@@ -1,25 +1,28 @@
+const { Queue } = require('../queue')
 
+let cats = new Queue()
 
-const CatService = {
-  
-  cats: [
-    {
-      image: 'https://scontent.find1-1.fna.fbcdn.net/v/t1.0-9/30798_102597843119736_4949205_n.jpg?_nc_cat=104&_nc_oc=AQlqNjxordoXL50j6YntAX2OiP9vE9K7mgiZqHY0iPGV7bZKncB0Vk1v_m-th0r3gJ0&_nc_ht=scontent.find1-1.fna&oh=e72f72611a1f5ec0728c510af8e54223&oe=5DDE13CB',
-      name: 'Sox',
-      gender: 'Female',
-      age: 12,
-      breed: 'Domestic Short-hair',
-      story: 'Owner could no longer take care of her'
-    },
-    {
-      image: '',
+cats.enqueue({
+  image: 'https://scontent.find1-1.fna.fbcdn.net/v/t1.0-9/30798_102597843119736_4949205_n.jpg?_nc_cat=104&_nc_oc=AQlqNjxordoXL50j6YntAX2OiP9vE9K7mgiZqHY0iPGV7bZKncB0Vk1v_m-th0r3gJ0&_nc_ht=scontent.find1-1.fna&oh=e72f72611a1f5ec0728c510af8e54223&oe=5DDE13CB',
+  name: 'Sox',
+  gender: 'Female',
+  age: 12,
+  breed: 'Domestic Short-hair',
+  story: 'Owner could no longer take care of her'
+})
+
+cats.enqueue({
+  image: '',
       name: 'Miss Prissy',
       gender: 'Female',
       age: 4,
       breed: 'Domestic Short-hair',
       story: 'Owner could no longer take care of her'
-    }
-  ],
+})
+
+const CatService = {
+  
+  cats,
   
   getCats() {return this.cats;},
 };
--- Petful-server/src/dogs/dogs-service.js
+++ Petful-server/src/dogs/dogs-service.js
@@ -1,27 +1,31 @@
+const { Queue } = require('../queue')
 
+let dogs = new Queue()
+dogs.enqueue(
+  {
+    image: 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1y0laje_50411b56.jpeg?height=354&region=0%2C0%2C400%2C225&width=630',
+    name: 'Old Yeller',
+    gender: 'Male',
+    age: 5,
+    breed: 'Yellow Mastador',
+    story: 'Owner passed away'
+  }
+)
+
+dogs.enqueue({
+  image: '',
+  name: 'Casey',
+  gender: 'Female',
+  age: 10,
+  breed: 'Black lab',
+  story: 'Thrown out on the street'
+})
 
 const DogService = {
-  
-  dogs: [
-    {
-      image: 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1y0laje_50411b56.jpeg?height=354&region=0%2C0%2C400%2C225&width=630',
-      name: 'Old Yeller',
-      gender: 'Male',
-      age: 5,
-      breed: 'Yellow Mastador',
-      story: 'Owner passed away'
-    },
-    {
-      image: '',
-      name: 'Casey',
-      gender: 'Female',
-      age: 10,
-      breed: 'Black lab',
-      story: 'Thrown out on the street'
-    },
-  ],
-  
-  getDogs() {return this.dogs;},
+
+  dogs,
+
+  getDogs() { return this.dogs; },
 };
 
 
